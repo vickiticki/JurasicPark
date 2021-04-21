f3304a504736f8f7bc660e35b8e1eed87b679f92
Victoria Tiller <vijotiller@gmail.com>
2021-04-21 11:37:08 -0400

    getting started

--- Program.cs
+++ Program.cs
@@ -2,11 +2,46 @@
 
 namespace JurasicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Dinosaur
     {
-      Console.WriteLine("Welcome to C#");
+
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dino Time");
+
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("(A)dd a dinosaur, (V)iew all dinosaurs, (R)emove a dinosaur, (T)ransfer a dinosaur, see a (S)ummary, or (Q)uit.");
+            var choice = Console.ReadLine().ToUpper();
+
+            switch (choice)
+            {
+                case "A":
+                    Console.WriteLine("Add dino");
+                    break;
+                case "V":
+                    Console.WriteLine("View dino");
+                    break;
+                case "R":
+                    Console.WriteLine("Remove dino");
+                    break;
+                case "T":
+                    Console.WriteLine("Transfer dino");
+                    break;
+                case "S":
+                    Console.WriteLine("Summary dino");
+                    break;
+                case "Q":
+                    Console.WriteLine("Quit");
+                    break;
+                default:
+                    Console.WriteLine("Try again");
+                    break;
+
+            }
+
+        }
+    }
 }
